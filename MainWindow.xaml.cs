aed5cb605a6562b735ec94d435dca191ea31271b
Ryusuke Tadagawa <r.tadagawa@gmail.com>
2022-01-17 21:10:09 +0900

    XAMLからの印刷を追加

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -112,10 +112,81 @@ namespace WPFPrintPages
             }
             streamXPS.Position = 0;
             var acount = "";
-            var filePath = $@"C:\Users\{acount}\Desktop\hogehoge.pdf";
+            var filePath = $@"C:\Users\{acount}\Desktop\テスト印刷.pdf";
             XpsPrintHelper.Print(streamXPS, queue.Name, "jobname", false, filePath);
         }
 
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+
+            var server = new LocalPrintServer();
+            var queue = server.GetPrintQueues().FirstOrDefault(_ => _.Name == "Microsoft Print to PDF");
+
+            // 用紙サイズ(A4)と印刷方向(縦)を設定します。
+            var ticket = queue.DefaultPrintTicket;
+            ticket.PageMediaSize = new PageMediaSize(PageMediaSizeName.ISOA4);
+            ticket.PageOrientation = PageOrientation.Portrait;
+
+            // 余白の上と左位置を取得します。
+            var area = queue.GetPrintCapabilities().PageImageableArea;
+            var originHeight = area.OriginHeight;
+            var originWidth = area.OriginWidth;
+
+            FixedDocument document = new FixedDocument();
+
+            var p = new TestPage();
+            FixedPage fp = CreateFixedPage(p);
+            PageContent pc = new PageContent();
+            pc.Child = fp;
+            document.Pages.Add(pc);
+
+            System.IO.MemoryStream streamXPS = new System.IO.MemoryStream();
+            using (Package pack = Package.Open(streamXPS, System.IO.FileMode.CreateNew))
+            {
+                using (var doc = new System.Windows.Xps.Packaging.XpsDocument(pack, CompressionOption.SuperFast))
+                {
+                    var writer = System.Windows.Xps.Packaging.XpsDocument.CreateXpsDocumentWriter(doc);
+                    writer.Write(document, ticket);
+                }
+            }
+            streamXPS.Position = 0;
+            var acount = "";
+            var filePath = $@"C:\Users\{acount}\Desktop\テスト印刷2.pdf";
+            XpsPrintHelper.Print(streamXPS, queue.Name, "jobname", false, filePath);
+
+
+        }
+
+
+        private static FixedPage CreateFixedPage(Page page)
+        {
+            Action dummy = () => { };
+            var disp = System.Windows.Threading.Dispatcher.CurrentDispatcher;
+            FixedPage fixpage = new FixedPage();
+            {
+                Frame fm = new Frame();
+
+                fm.Content = page;
+                disp.Invoke(dummy, System.Windows.Threading.DispatcherPriority.Loaded);//バインディングとかいろいろさせる必要がある場合
+
+                FixedPage.SetLeft(fm, 0);
+                FixedPage.SetTop(fm, 0);
+
+                fixpage.Children.Add(fm);
+
+                Size sz = new Size(page.Width, page.Height);
+
+                fixpage.Width = page.Width;
+                fixpage.Height = page.Height;
+
+                fixpage.Measure(sz);
+                fixpage.Arrange(new Rect(new Point(), sz));
+                fixpage.UpdateLayout();
+
+
+            }
+            return fixpage;
+        }
 
     }
 }
